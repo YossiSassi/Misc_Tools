7bf3a577c02b5c5b7904ea759edb28f81c71962a
Y1nTh35h3ll <yossis@protonmail.com>
2021-11-18 11:21:47 +0200

    removed APIPA, and note about lack of PSv2.0 support (for now, missing .net methods)

--- Ping ASync fast - Domain or IP range.ps1
+++ Ping ASync fast - Domain or IP range.ps1
@@ -1,9 +1,22 @@
 ## FAST(!) ping Async - for Domain-wide & non-domain networks ##
-# Done with "pure" living off the land mindset - no dependencies/Modules needed. supports Powershell v2.0+.
+# Done with pure 'living off the land' mindset - no dependencies/Modules needed. sadly needs SendPingAsync in .net to properly powershell v2.0+.
 # Comments to yossis@protonmail.com (1nTh35h311)
 
-# get my current ipv4
-$i=(ipconfig.exe| Select-String ipv4)[0].ToString().Split(":")[1].Trim().Split("."); $IPRange="$($i[0]).$($i[1]).$($i[2])";
+# Since powershell v2.0 (default of Windows 7/2008R2) misses needed .net Types and methods, separate methods are used for PSv2.0 vs. Other Windows PowerShell versions
+if ($PSVersionTable.PSVersion.Major -eq 2)
+    {
+        "Missing supported .net methods to run Ping async. Consider running again from Powershell version greater than 2.0. Quiting.";
+        Exit;
+}
+
+# get my current ipv4 addresses, excluding APIPA
+$IPRange = @();
+$i = (ipconfig.exe | Select-String IPv4 | Where {$_ -notlike "*169.254.*"}) | foreach { 
+        $splitIP = $_.ToString().Split(":")[1].Trim().Split("."); 
+        $ThreeOctets="$($splitIP[0]).$($splitIP[1]).$($splitIP[2])";
+        $IPRange += $ThreeOctets;
+        Clear-Variable ThreeOctets, splitIP
+    }
 
 # See if running from a domain-joined host or not
 $Role = Get-WmiObject -Class Win32_ComputerSystem | select -ExpandProperty DomainRole;
@@ -24,12 +37,12 @@ $DomainRoles = 1,3,4,5;
 if ($DomainRoles -contains $Role) {
         "Running on $ComputerRole (Domain identified)`n";
         $Domain = ([adsi]'').name.ToString().ToUpper();
-        $Option = Read-Host "Domain Or IP Range?`n1=Domain <$Domain>`n2=Current host's IP Range <$IPRange.1..254>`n3=Other IP Range`n";
+        $Option = Read-Host "Domain Or IP Range?`n1=Domain <$Domain>`n2=Current host's IP Range <$IPRange .1..254>`n3=Other IP Range`n";
     }
 else
     {
         "No Domain identified (Running on $ComputerRole)`n";
-        $Option = Read-Host "Domain Or IP Range?`n1=Domain <N/A from this host - Ignore>`n2=Current host's IP Range <$IPRange.1..254>`n3=Other IP Range`n";
+        $Option = Read-Host "Domain Or IP Range?`n1=Domain <N/A from this host - Ignore>`n2=Current host's IP Range <$IPRange .1..254>`n3=Other IP Range`n";
     }
 
 switch ($Option)
@@ -50,7 +63,17 @@ switch ($Option)
 
         # current IP range
         2 {
-            $HostsToCheck = 1..254 | % {"$IPRange.$_"};
+            if ($($IPRange.Count) -eq 1)
+                {
+                    $HostsToCheck = 1..254 | % {"$IPRange.$_"};
+                }
+            else
+                {
+                    [int]$count = 0;
+                    $Options = $IPRange | ForEach-Object {"$count - $_`n";$count++}
+                    [int]$OptionIP = Read-Host "Choose from current host's IP Range`n$Options`n";
+                    $HostsToCheck = 1..254 | % {"$($IPRange[$OptionIP]).$_"};
+                }
            }
 
         # other IP range
@@ -66,9 +89,11 @@ switch ($Option)
 $EAP = $ErrorActionPreference;
 $ErrorActionPreference = "Silentlycontinue";
 
+"Checking $($HostsToCheck.count) IPs..."
+
+## for Newer PowerShell versions than 2.0
 # CAN CHANGE THIS ping timeout to be much less or perhaps more?
 $TimeOutInMiliSeconds = 200;
-"Checking $($HostsToCheck.count) IPs..."
 
 $PingResults = $HostsToCheck | foreach {(New-Object System.Net.NetworkInformation.Ping).SendPingAsync($_,$TimeOutInMiliSeconds)};
 [Threading.Tasks.Task]::WaitAll($PingResults);
@@ -261,6 +286,58 @@ measure-command {[PingEx.NetworkInformationExtensions]::PingAsync($HostsToCheck,
 
 $IPs = ([PingEx.NetworkInformationExtensions]::PingAsync($HostsToCheck, [TimeSpan]::FromMilliseconds(1), $false).Result | Where-Object IsSuccess -eq "True").ipaddress.IPAddressToString;
 "Second iteration method found $($IPs.count) responding IPs.";
+
+<# NOTE: can also consider using this function (eventually, pingAsync as well..)
+Function global:Invoke-KingPong{
+#.Synopsis
+#   King Pong - The Ping Kong...
+#.DESCRIPTION
+#   Power Ping: Multi-NIC/Auto-Subnet/Multi-Thread.
+#   Returns only IPs where Success.
+#   Inspired by a tweet from @Lee_Holmes.
+#.LINK
+#   https://twitter.com/Lee_Holmes/status/646890380995067904
+    [CmdletBinding()]
+    [Alias('Pong')]
+    Param()
+    Begin{
+        ## Subs
+        # IP to Num
+        function IP2NUM{Param([String]$IP); return [int64]([int64]$IP.split(".")[0]*16777216+[int64]$IP.split(".")[1]*65536+[int64]$IP.split(".")[2]*256+[int64]$IP.split(".")[3])} 
+        # Num to IP
+        function NUM2IP{Param([Int64]$Num); return (([math]::truncate($Num/16777216)).tostring()+"."+([math]::truncate(($Num%16777216)/65536)).tostring()+"."+([math]::truncate(($Num%65536)/256)).tostring()+"."+([math]::truncate($Num%256)).tostring())}
+        ## Prep
+        # Prep empty IP list
+        $IPList = @()
+        # Get NICs
+        $NICList = Gwmi win32_networkadapterconfiguration | Where defaultIPGateway -ne $null
+        }
+    Process{
+        # List IPs to Ping
+        Foreach($NIC in $NICList){
+            # IP & Mask
+            $IP   = [Net.IPAddress]::Parse($NIC.ipaddress[0])
+            $Mask = [Net.IPAddress]::Parse($NIC.ipsubnet[0])
+            # Network & Broadcast
+            $Ntwrk  = New-Object net.IPAddress ($Mask.address -band $IP.address)
+            $Brdcst = New-Object Net.IPAddress ([Net.IPAddress]::parse("255.255.255.255").address -bxor $Mask.address -bor $Ntwrk.address)
+            Write-Verbose "$($Ntwrk.IPAddressToString) / $Mask" 
+            # Start & End
+            $Start = (IP2NUM -IP $ntwrk.ipaddresstostring)  +1
+            $End   = (IP2NUM -IP $brdcst.ipaddresstostring) -1
+            # Add IPs to IPList
+            For($n=$Start; $n -le $End; $n++){$IPList += NUM2IP -Num $n}
+            }
+        # Ping IP List
+        $Ping = $IPList | sort -Unique | %{(New-Object Net.NetworkInformation.Ping).SendPingAsync($_,250)}
+        [Threading.Tasks.Task]::WaitAll($Ping) 
+        # Get IPs where Success
+        $Success = ($Ping.Result | ? Status -eq Success | Select -ExpandProp address).IPAddressToString
+        Write-Verbose "Success: $($Success.count) / $($Ping.Result.count)"
+        }
+    End{return $Success}
+    } 
+#>
 
 # Combine outputs from BOTH methods for ideal results
 $IPs += $SuccessIPs; $IPs = $IPs | Where-Object {$_ -match '(\d{1,3}\.){3}\d{1,3}'} | select -Unique;
